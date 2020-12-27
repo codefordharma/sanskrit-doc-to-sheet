98a8bd97abe0df5777bee1eeabe4bba8b5022bc3
Himank Barve <hbarve1592@gmail.com>
2020-12-27 12:12:12 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sanskrit Doc to Sheet
+
+Description: This repo is for converting .txt file of Sanskrit literature (shlokas) to .csv file so that we can work on Google Sheet.
+
+## Pre-requisites
+
+* Node.js > v12.x
+
+## Input Data
+
+* Input data should be in `/data` folder which is kept ignored as we are keeping all the sanskrit data in Google Doc and Google Sheet.
+* Input Data file should be in `.txt` file format.
+
+## Output Data
+
+* Output will be generated in the same `/data` folder with same name as input file but with extention of .csv
+
+**NOTE:** output depends on the input text file, the more input file is structured the better will be the result.
+
+## Issues
+
+* all the suggestions and input are welcome in issue board.
+
+Thank You.
+
+धर्मो रक्षति रक्षितः
---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+/**
+ * This function converts .txt file of sanskrit to .csv file
+ * 
+ * @param {string} fileName - filename should be here which is kept in `/data` folder in .txt format 
+ */
+async function txt_to_csv (fileName) {
+    try {
+        const data = await fs.readFileSync(`./data/${fileName}.txt`, 'utf8');
+
+    const arrayList = data.split('।। \r\n')
+        .map(value => value.split('।\r\n').join('। '))
+        .map(value => value.split('\r\n').filter(v => v.length !== 0).join('\r\n'))
+        .join(',')
+
+        try {
+            await fs.writeFileSync(`./data/${fileName}-${Date.now()}.csv`, arrayList)
+        } catch (error) {
+            console.error("There is an Error writing the file.", error);
+        }
+    } catch (error) {
+        console.error("There is an Error reading the file.", error);
+    }
+}
+
+/**
+ * call function here to with exact file name to generate .csv file
+ * 
+ * NOTE: keep no space in between file name.
+ */
+// txt_to_csv('valmiki-ramayana');
